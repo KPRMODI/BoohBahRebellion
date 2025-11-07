6cb8d5467d5d9f077178f69f2e625d8e8c500874
KPRMODI <kpmodi@my.waketech.edu>
2025-11-07 10:08:18 -0500

    last minute fix for jumbah im done

--- Jumbah.java
+++ Jumbah.java
@@ -1,8 +1,8 @@
 public class Jumbah extends SupremeBoohbah{
     public Jumbah(){
+        super();
         name = "Jumbah";
-        super.command("We will rebel!"); // from the instructions I assumed you had to call the overloaded method but it looks different from everything else
-        // It also said to print before the rebellion statements so I assumed that this was correct according to the instructions
+        System.out.println(name + " Refuses to obey!"); // he refuses to obey
     }
 
     @Override
